8c3e414fe0d2d1acf19d0bd05a55963a078078c3
Robert Gallegos <rlgallegos85@gmail.com>
2023-01-09 05:45:48 -0500

    Objects Lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "555 Myrtle"
+};
+
+/*updateEmployeeWithKeyAndValue(): this function should take in three arguments: 
+an employee Object, a key and a value. 
+This function should not mutate the employee;
+ it should return a new Object that has an updated value for the key passed in. 
+ Hint: use the spread operator!*/
+
+ function updateEmployeeWithKeyAndValue(object, key, value)
+ {
+    //copy object
+    const newEmployee = {...object};
+    //update new object
+    newEmployee[key] = value;
+    //return value
+    return newEmployee;
+ }
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value)
+{
+    object[key] = value;
+    return object;
+}
+//deletes key from clone of employee and returns new object
+//non-destructive
+function deleteFromEmployeeByKey(object, key)
+{
+    const newEmployee = {...object};
+    delete newEmployee[key];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(object, key)
+{
+    delete object[key];
+    return object;
+}
